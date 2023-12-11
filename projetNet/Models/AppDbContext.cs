48c7bd6539276a440226fa2b1faf799eda10ca01
mariem.benayed <mariem.benayed@intern.continuousnet.com>
2023-12-11 01:47:41 +0100

    first commit

---
+++ projetNet/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projetNet.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
